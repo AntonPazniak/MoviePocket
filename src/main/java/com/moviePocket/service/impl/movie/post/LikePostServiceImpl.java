e420b5fd090bfcb396b12358ecb220abbc9a0ec0
Danila <danpry@st.amu.edu.pl>
2023-11-11 15:46:56 +0100

    Creation of services implementations  for post module

---
+++ src/main/java/com/moviePocket/service/impl/movie/post/LikePostServiceImpl.java
@@ -0,0 +1,75 @@
+package com.moviePocket.service.impl.movie.post;
+
+import com.moviePocket.entities.movie.post.LikePost;
+import com.moviePocket.entities.movie.post.Post;
+import com.moviePocket.entities.user.User;
+import com.moviePocket.repository.movie.post.LikePostRepository;
+import com.moviePocket.repository.movie.post.PostRepository;
+import com.moviePocket.repository.user.UserRepository;
+import com.moviePocket.service.movie.post.LikePostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LikePostServiceImpl implements LikePostService {
+
+    @Autowired
+    private LikePostRepository likePostRepository;
+    @Autowired
+    private PostRepository postRepository;
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public ResponseEntity<Void> setLikeOrDisOrDel(String username, Long id, boolean likeOrDis) {
+        Post post = postRepository.getById(id);
+        User user = userRepository.findByEmail(username);
+        LikePost likePost = likePostRepository.getByUserAndPost(user, post);
+        if (user == null)
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        else if (post == null)
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        else {
+            if (likePost == null) {
+                likePostRepository.save(new LikePost(post, user, likeOrDis));
+            } else if (likePost.isLickOrDis() == likeOrDis) {
+                likePostRepository.delete(likePost);
+            } else {
+                likePost.setLickOrDis(likeOrDis);
+                likePostRepository.save(likePost);
+            }
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<boolean[]> getLikeOrDis(String username, Long id) {
+        Post post = postRepository.getById(id);
+        User user = userRepository.findByEmail(username);
+        LikePost likePost = likePostRepository.getByUserAndPost(user, post);
+        if (user == null)
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        if (post == null)
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        if (likePost != null) {
+            return ResponseEntity.ok(new boolean[]{likePost.isLickOrDis()});
+        } else {
+            return ResponseEntity.ok(new boolean[]{});
+        }
+    }
+
+    @Override
+    public ResponseEntity<int[]> getAllLikeAndDisByIdPost(Long idPost) {
+        Post post = postRepository.getById(idPost);
+        if (post != null) {
+            return ResponseEntity.ok(new int[]{
+                    likePostRepository.countByPostAndLickOrDisIsTrue(post),
+                    likePostRepository.countByPostAndLickOrDisIsFalse(post)
+            });
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
---
+++ src/main/java/com/moviePocket/service/impl/movie/post/PostServiceImpl.java
@@ -0,0 +1,161 @@
+package com.moviePocket.service.impl.movie.post;
+
+import com.moviePocket.entities.movie.post.MovieListInPost;
+import com.moviePocket.entities.movie.post.ParsPost;
+import com.moviePocket.entities.movie.post.Post;
+import com.moviePocket.entities.user.User;
+import com.moviePocket.repository.movie.post.LikePostRepository;
+import com.moviePocket.repository.movie.post.MovieListInPostRepository;
+import com.moviePocket.repository.movie.post.PostRepository;
+import com.moviePocket.repository.user.UserRepository;
+import com.moviePocket.service.movie.post.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PostServiceImpl implements PostService {
+
+    private final PostRepository postRepository;
+    private final UserRepository userRepository;
+    private final MovieListInPostRepository movieListInPostRepository;
+    private final LikePostRepository likePostRepository;
+
+
+    @Override
+    public ResponseEntity<Void> setPost(String email, String title, String content) {
+        User user = userRepository.findByEmail(email);
+        if (user == null)
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        Post post = new Post(user, content, title);
+        postRepository.save(post);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<Void> updatePostTitle(String email, Long idPost, String title) {
+        User user = userRepository.findByEmail(email);
+        Post post = postRepository.getById(idPost);
+        if (user == null)
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        else if (post == null)
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        else if (post.getUser() != user) {
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        } else {
+            post.setTitle(title);
+            postRepository.save(post);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+    }
+
+    @Override
+    public ResponseEntity<Void> updatePostContent(String email, Long idPost, String content) {
+        User user = userRepository.findByEmail(email);
+        Post post = postRepository.getById(idPost);
+        if (user == null)
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        else if (post == null)
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        else if (post.getUser() != user) {
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        } else {
+            post.setContent(content);
+            postRepository.save(post);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+    }
+
+    @Override
+    public ResponseEntity<Void> deletePost(String email, Long idPost) {
+        User user = userRepository.findByEmail(email);
+        Post post = postRepository.getById(idPost);
+        if (user == null)
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        else if (post == null)
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        else if (post.getUser() != user) {
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        } else {
+            movieListInPostRepository.deleteAllByPost(post);
+            likePostRepository.deleteAllByMovieList(post);
+            postRepository.delete(post);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+    }
+
+    @Override
+    public ResponseEntity<List<ParsPost>> getAllByTitle(String title) {
+        List<Post> posts = postRepository.findAllByTitle(title);
+        return ResponseEntity.ok(parsPost(posts));
+    }
+
+    @Override
+    public ResponseEntity<List<ParsPost>> getPost(Long idPost) {
+        if (postRepository.existsById(idPost)) {
+            Post post = postRepository.getById(idPost);
+            List<Post> posts = new ArrayList<>();
+            posts.add(post);
+            return ResponseEntity.ok(parsPost(posts));
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @Override
+    public ResponseEntity<List<ParsPost>> getAllPosts() {
+        List<Post> posts = postRepository.findAll();
+        return ResponseEntity.ok(parsPost(posts));
+    }
+
+    @Override
+    public ResponseEntity<List<ParsPost>> getAllMyPosts(String email) {
+        User user = userRepository.findByEmail(email);
+        if (user == null)
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        else {
+            List<Post> posts = postRepository.findAllByUser(user);
+            return ResponseEntity.ok(parsPost(posts));
+        }
+    }
+
+    @Override
+    public ResponseEntity<List<ParsPost>> getAllByUsernamePosts(String username) {
+        User user = userRepository.findByUsernameAndAccountActive(username, true);
+        if (user == null)
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        else {
+            List<Post> posts = postRepository.findAllByUser(user);
+            return ResponseEntity.ok(parsPost(posts));
+        }
+    }
+
+    private List<ParsPost> parsPost(List<Post> posts) {
+        List<ParsPost> parsPostLL = new ArrayList<>();
+        for (Post post : posts) {
+            List<MovieListInPost> movieListInPostList = movieListInPostRepository.getAllByPost(post);
+            List<Long> idPost = new ArrayList<>();
+            for (MovieListInPost movieListInPost : movieListInPostList) {
+                idPost.add(movieListInPost.getIdMovieList());
+            }
+            int[] likeAndDis = new int[]{likePostRepository.countByPostAndLickOrDisIsTrue(post),
+                    likePostRepository.countByPostAndLickOrDisIsFalse(post)};
+            ParsPost parsPost = new ParsPost(
+                    post.getId(),
+                    post.getTitle(),
+                    post.getContent(),
+                    idPost,
+                    likeAndDis,
+                    post.getUser().getUsername(),
+                    post.getCreated(),
+                    post.getUpdated()
+            );
+            parsPostLL.add(parsPost);
+        }
+        return parsPostLL;
+    }
+}
