7a94be23e151bf6c4af5ce271b17ef11eafb6a0c
Danila <danpry@st.amu.edu.pl>
2023-12-07 00:52:34 +0100

    adding new field to db and user table

--- src/main/java/com/moviePocket/entities/user/User.java
+++ src/main/java/com/moviePocket/entities/user/User.java
@@ -2,6 +2,7 @@ package com.moviePocket.entities.user;
 
 
 import com.moviePocket.entities.BaseEntity;
+import com.moviePocket.entities.image.ImageEntity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -38,22 +39,26 @@ public class User extends BaseEntity {
     @Column(nullable = false)
     private boolean accountActive;
 
+    @OneToOne
+    @JoinColumn(name = "idAvatar", referencedColumnName = "id")
+    private ImageEntity avatar;
+
+
     @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinTable(
             name = "users_roles",
             joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")},
             inverseJoinColumns = {@JoinColumn(name = "role_id", referencedColumnName = "id")}
     )
-
-
     private List<Role> roles = new ArrayList<>();
 
-    public User(String username, String email, String password,  List<Role> roles) {
+    public User(String username, String email, String password, List<Role> roles) {
         this.username = username;
         this.password = password;
         this.email = email;
         this.roles = roles;
         this.emailVerification = false;
         this.accountActive = true;
+        this.avatar = null;
     }
 }
